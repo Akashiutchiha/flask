039d772f3be6794c85cd5fddd18b1d9934529024
Ndongmo Christian <christianhonore2003@gmail.com>
2022-11-17 21:37:15 +0100

    Feat: View for Working with Forms

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World</h1>'
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
